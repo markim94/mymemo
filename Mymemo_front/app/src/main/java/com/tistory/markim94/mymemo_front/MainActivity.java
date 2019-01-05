5429666f432aafa12624dabb9d60b848f36c450a
markim94 <markim94@naver.com>
2019-01-05 20:22:24 +0900

    create project file

---
+++ Mymemo_front/app/src/main/java/com/tistory/markim94/mymemo_front/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tistory.markim94.mymemo_front;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
